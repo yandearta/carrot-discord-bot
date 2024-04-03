1d965785f22cf1acbdc8cfc4f8293e8f6c57a74d
Yande Arta <yandearta00@gmail.com>
2024-04-03 13:51:42 +0800

    Update response text format

--- src/commands/pilih.ts
+++ src/commands/pilih.ts
@@ -27,12 +27,20 @@ export async function run({ interaction }: SlashCommandProps) {
   const pickedChoice =
     uniqueChoices[Math.floor(Math.random() * uniqueChoices.length)];
 
+  const pickedChoiceFmt =
+    pickedChoice.startsWith("<@") && pickedChoice.endsWith(">")
+      ? pickedChoice
+      : `\`${pickedChoice}\``;
+
   const optionsText = uniqueChoices
+    .map((choice) =>
+      choice.startsWith("<@") && choice.endsWith(">") ? choice : `\`${choice}\``
+    )
     .join(", ")
     // Replace the last comma with a ampersand
     .replace(/,\s([^,]+)$/, " & $1");
 
-  const replyText = `Dari opsi ${optionsText}, aku memilih ${pickedChoice}!`;
+  const replyText = `Dari opsi ${optionsText}, aku memilih ${pickedChoiceFmt}!`;
 
   await interaction.reply(replyText);
 }
